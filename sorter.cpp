070b89844c2813de8ddd2c98957da2d46656a105
jbiz <pozolotusiko@mail.ru>
2022-03-07 21:00:57 +0300

    Checker for sortings added

--- sorter.cpp
+++ sorter.cpp
@@ -10,6 +10,17 @@ void sorter::print(int *arrToSort, int N)
    for(int i = 0; i < N;i++) qDebug() << arrToSort[i];
 }
 
+bool isSorted(int *arrToSort, int N)
+{
+    int checker = arrToSort[0];
+    for(int i = 1; i < N; i++)
+    {
+        if(arrToSort[i] < checker) return false;
+        checker = arrToSort[i];
+    }
+    return true;
+}
+
 void sorter::swap(int &par1, int &par2)
 {
     int tmp = par1;
