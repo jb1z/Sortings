26479f69b267bd369d25bf9913f82cc923f9e0ed
jbizLT <pozolotusiko1@outlook.com>
2022-03-12 17:53:27 +0300

    An ui view first version done

--- countsorter.h
+++ countsorter.h
@@ -1,11 +1,12 @@
 #ifndef COUNTSORTER_H
 #define COUNTSORTER_H
+#include "sorter.h"
 
-
-class countsorter
+class countsorter: private Sorter
 {
 public:
     countsorter();
+    void sort(int arrToSort[], int m, int n);
 };
 
 #endif // COUNTSORTER_H
